fd7386b2eb0911998b1c0471400bd12375fa257c
omiq <chris@chrisg.com>
2024-10-22 21:46:12 +0100

    title

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
+// deno run --allow-all --unstable index.ts
+
+
+
+
 import { WebUI } from "https://deno.land/x/webui/mod.ts";
 
 const win = new WebUI();
-win.show('<html><script src="webui.js"></script> Hello World from Deno! </html>');
+win.show('<html><head><title>Deno World</title><script src="webui.js"></script></head><body> Hello World from Deno! </html>');
 await WebUI.wait();
